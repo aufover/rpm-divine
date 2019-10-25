5e89bc75b5da2a2820c8adf0765502bcdbc5712a
Lukas Zaoral <lzaoral@redhat.com>
2019-10-25 12:53:20 +0200

    do_SRPM.sh: Add an SRPM build script

---
+++ do_SRPM.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+SPEC=${SPEC:-../divine.spec}
+
+rm -rf srpm
+mkdir srpm
+cd srpm || exit $?
+
+# clone git repo
+git clone https://gitlab.fi.muni.cz/paradise/mirror/divine.git
+
+# make package
+pushd divine
+VER=`sed "s/-/+/g" <(git describe)`
+PREFIX="divine-$VER/"
+
+echo Making divine-$VER.tar.gz ...
+git archive --prefix=$PREFIX -o ../divine-$VER.tar.gz HEAD
+popd
+
+# update version
+echo Updating $SPEC ...
+sed -i "s/^Version:.*/Version:        $VER/" $SPEC
+
+if [ -v CHECK ]; then
+  sed '/make unit/a make functional' $SPEC > divine_check.spec
+  SPEC=divine_check.spec
+fi
+
+# copy patches
+cp ../*.patch .
+
+# build SRPM
+echo Building SRPM ...
+rpmbuild -bs $SPEC --define "_sourcedir $PWD" --define "_srcrpmdir $PWD"
