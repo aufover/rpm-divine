2007b584f93d305e1b9957614817125a9330ba17
Lukas Zaoral <lzaoral@redhat.com>
2019-11-14 10:40:17 +0100

    divine2csgrep.py: Beautify the code a little bit

--- divine2csgrep.py
+++ divine2csgrep.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-import re
 import sys
 import yaml
 
@@ -19,21 +18,25 @@ with open(sys.argv[1] + ".csgrep", "w") as stream:
         sys.exit(0)
 
     for frame in report["active stack"]:
-        if "/opt/divine" in frame["location"]:
+        if "/opt/divine/" in frame["location"]:
             continue
    
-        key_location = frame["location"].split(":")
-        print(key_location[0] + ": In funtion ‘" + frame["symbol"] + "’:", file=stream)
+        location = frame["location"].split(":")
+        print(location[0] + ": In funtion ‘" + frame["symbol"] + "’:",
+                file=stream)
         
-        key_location = key_location[0] + ":" + key_location[1]
+        location = location[0] + ":" + location[1]
         trace = report["error trace"].split("\n")
         trace.pop() # might break in future
         for line in trace:
-            print(key_location + (": error: DIVINE " if line is trace[0] else ": note: ") + line, file=stream)
+            print(location +
+                    (": error: DIVINE " if line is trace[0] else ": note: ")
+                    + line, file=stream)
 
-        print(key_location + ": note: backtrace:", file=stream)
+        print(location + ": note: backtrace:", file=stream)
         break
 
     for frame in report["active stack"]:
         location = frame["location"].split(":")
-        print(location[0] + ":" + location[1] + ": note: " + frame["symbol"], file=stream)
+        print(location[0] + ":" + location[1] + ": note: " + frame["symbol"],
+                file=stream)
