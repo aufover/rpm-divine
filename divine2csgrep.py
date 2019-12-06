dbbf2dea90cf5b04f9752b6987a4e3ca4ad377ec
Lukas Zaoral <lzaoral@redhat.com>
2019-12-06 10:35:48 +0100

    divine2csgrep.py: Small fixes and major refactor
    Changes:
        - Fix typo in stack trace
        - Code is now PEP8 compliant
        - Add type annotations
        - Add support for "unknown" error type
        - General refactor

--- divine2csgrep.py
+++ divine2csgrep.py
@@ -1,15 +1,33 @@
 #!/usr/bin/python3
 
+from typing import List, TextIO
+
 import sys
 import yaml
 
 
-def main():
+def print_error_trace(report, error_type: str,
+                      location: str = None) -> None:
+    trace: List[str] = report["error trace"].split("\n")
+    trace.pop()  # might break in future
+
+    if location is None:
+        location = report["input file"]
+
+    for line in trace:
+        print(location + (": error: " + error_type + ": " if line is trace[0]
+                          else ": note: ") + line)
+
+    return
+
+
+def main() -> None:
     if len(sys.argv) != 2:
         print("USAGE: %s [DIVINE REPORT]" % sys.argv[0], file=sys.stderr)
         sys.exit(1)
 
-    in_file = sys.stdin if sys.argv[1] == "-" else open(sys.argv[1], "r")
+    in_file: TextIO = (sys.stdin if sys.argv[1] == "-"
+                       else open(sys.argv[1], "r"))
 
     try:
         report = yaml.safe_load(in_file)
@@ -18,39 +36,33 @@ def main():
         sys.exit(1)
 
     if report is None:
-        print("divine: error: DIVINE FAULT: Divine crashed and no log was created")
-        print("divine: note: see stderr output of given divine process")
+        print("divine: error: DIVINE FAULT: Divine crashed and no log was"
+              " created\ndivine: note: see stderr output of given divine"
+              " process")
         return
 
     if not report["error found"]:
         return
 
-    if report["error found"] == "boot":
-        trace = report["error trace"].split("\n")
-        trace.pop()  # might break in future
+    if report["error found"] == "unknown":
+        print_error_trace(report, "DIVINE UNKNOWN")
+        return
 
-        for line in trace:
-            print(report["input file"] +
-                  (": error: DIOS BOOT: " if line is trace[0]
-                   else ": note: DIOS BOOT: ") + line)
+    if report["error found"] == "boot":
+        print_error_trace(report, "DIOS BOOT")
         return
 
     for frame in report["active stack"]:
         if "/opt/divine/" in frame["location"]:
             continue
 
-        location = frame["location"].split(":")
-        print(location[0] + ": In funtion ‘" + frame["symbol"] + "’:")
+        location: List[str] = frame["location"].split(":")
+        print(location[0] + ": In function ‘" + frame["symbol"] + "’:")
 
-        location = location[0] + ":" + location[1]
-        trace = report["error trace"].split("\n")
-        trace.pop()  # might break in future
+        location_string: str = location[0] + ":" + location[1]
+        print_error_trace(report, "DIVINE ERROR", location_string)
 
-        for line in trace:
-            print(location + (": error: DIVINE " if line is trace[0]
-                              else ": note: ") + line)
-
-        print(location + ": note: backtrace:")
+        print(location_string + ": note: backtrace:")
         break
 
     for frame in report["active stack"]:
@@ -58,6 +70,7 @@ def main():
         print(location[0] + ":" + location[1] + ": note: " + frame["symbol"])
 
     in_file.close()
+
 
 if __name__ == "__main__":
     main()
