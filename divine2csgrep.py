e917d0ac3c1260dcf7c5d59f7f14a7b6dcb9a3e1
Lukas Zaoral <lzaoral@redhat.com>
2020-02-03 13:28:33 +0100

    divine2csgrep: Rework the output format
    * Add support for memory leak reports
    * Add support for symbolic mode reports
    * Some uninteresting information are now omitted by default.
    * The `--verbose` option prints the output "as is".

--- divine2csgrep.py
+++ divine2csgrep.py
@@ -1,12 +1,35 @@
 #!/usr/bin/python3
 
-from typing import List, TextIO
+from enum import Enum
+from typing import List
 
+import argparse
+import re
 import sys
 import yaml
 
 
-def print_error_trace(report, error_type: str,
+class Error(Enum):
+    warning = 1
+    error = 2
+    fatal_error = 3
+
+
+def sanitise(line: str) -> str:
+    if "leaked" in line:
+        leak_count: int = len(re.findall(r"leaked", line))
+        return "FAULT: %d heap object%s leaked" % (leak_count,
+                                                   "" if leak_count == 1
+                                                   else "s")
+
+    return re.sub(
+            re.compile("((?<=heap\*)|(?<=alloca\*)|(?<=global\*))(.*(?= leaked)|[^]]+)||[0-9]+:",
+                       re.DOTALL),
+            "",
+            line)
+
+
+def print_error_trace(report, error_type: Error, verbose: bool,
                       location: str = None) -> None:
     trace: List[str] = report["error trace"].split("\n")
     trace.pop()  # might break in future
@@ -15,62 +38,69 @@ def print_error_trace(report, error_type: str,
         location = report["input file"]
 
     for line in trace:
-        print(location + (": error: " + error_type + ": " if line is trace[0]
-                          else ": note: ") + line)
+        if report.get("symbolic") is not None and "ASSUME" in line:
+            continue
+
+        if "FAULT" in line:
+            if not verbose:
+                line = sanitise(line)
+
+            print(location + ": " + error_type.name.replace("_", " ") + ": " +
+                  line)
+            continue
+
+        print(location + ": note: " + line.replace("[0] ", ""))
 
     return
 
 
-def main() -> None:
-    if len(sys.argv) != 2:
-        print("USAGE: %s [DIVINE REPORT]" % sys.argv[0], file=sys.stderr)
-        sys.exit(1)
-
-    in_file: TextIO = (sys.stdin if sys.argv[1] == "-"
-                       else open(sys.argv[1], "r"))
-
+def main(args: argparse.Namespace) -> None:
     try:
-        report = yaml.safe_load(in_file)
+        report = yaml.safe_load(args.infile)
+        args.infile.close()
     except yaml.YAMLError as exc:
         print(exc, file=sys.stderr)
         sys.exit(1)
 
     if report is None:
-        print("divine: error: DIVINE ERROR: Divine crashed and no log was"
-              " created\ndivine: note: see stderr output of given divine"
-              " process")
+        print("Error: DIVINE_WARNING:\n" +
+              "divine: fatal error: Divine crashed and no log was created\n" +
+              "divine: note: see stderr output of given divine process")
         return
 
     if not report["error found"]:
         return
 
+    print("Error: DIVINE_WARNING:")
     if report["error found"] == "unknown":
-        print_error_trace(report, "DIVINE UNKNOWN")
+        print_error_trace(report, Error.fatal_error, args.verbose)
         return
 
     if report["error found"] == "boot":
-        print_error_trace(report, "DIOS BOOT")
+        print_error_trace(report, Error.fatal_error, args.verbose)
         return
 
+    # FIXME: This is atrocious
     for frame in report["active stack"]:
         if "/opt/divine/" in frame["location"]:
             continue
-
-        location: List[str] = frame["location"].split(":")
-        print(location[0] + ": In function ‘" + frame["symbol"] + "’:")
-
-        location_string: str = location[0] + ":" + location[1]
-        print_error_trace(report, "DIVINE ERROR", location_string)
-
-        print(location_string + ": note: backtrace:")
         break
+
+    location: List[str] = frame["location"].split(":")
+    print(location[0] + ": scope_hint: In function ‘" + frame["symbol"] + "’:")
+    print_error_trace(report, Error.error, args.verbose,
+                      location[0] + ":" + location[1])
 
     for frame in report["active stack"]:
         location = frame["location"].split(":")
         print(location[0] + ":" + location[1] + ": note: " + frame["symbol"])
 
-    in_file.close()
-
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Convert Divine to csgrep")
+    parser.add_argument("-v", "--verbose", action="store_true",
+                        help="do not sanitise the error cause")
+    parser.add_argument("infile", nargs="?", type=argparse.FileType("r"),
+                        default=sys.stdin, help="input file (default: stdin)")
+
+    main(parser.parse_args())
