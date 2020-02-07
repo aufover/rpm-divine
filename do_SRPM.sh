13758ba323c636a4cc43a64ac0469d2c64b799a0
Lukas Zaoral <lzaoral@redhat.com>
2020-02-07 10:34:43 +0100

    do_SRPM: Remove creation of SRPM with testsuite enabled
    This is now replaced with GitLab CI

--- do_SRPM.sh
+++ do_SRPM.sh
@@ -16,12 +16,10 @@ git clone --quiet https://gitlab.fi.muni.cz/paradise/mirror/divine.git
 # make package
 pushd divine > /dev/null
 TAG=$(git describe | sed -e "s/-/_/g")
+PREFIX="divine-$TAG/"
 
-for NAME in "divine-$TAG" "divine-tested-$TAG"; do
-  echo "Making $NAME.tar.gz..."
-  git archive --prefix="$NAME/" -o "../$NAME.tar.gz" HEAD
-done
-
+echo "Making divine-$TAG.tar.gz..."
+git archive --prefix="$PREFIX" -o "../divine-$TAG.tar.gz" HEAD
 popd > /dev/null
 
 # update version
